bd2713fc94e433b561e25436cf393dcf8832e0a9
Nigel Karunaratne <nigel.karunaratne36@gmail.com>
2025-11-12 13:44:14 -0500

    Implement some Workspace functions, alter TagFile struct

--- tagcore/src/tagfile.rs
+++ tagcore/src/tagfile.rs
@@ -1,4 +1,44 @@
-#[derive(Debug)]
+use std::{collections::HashMap, collections::HashSet, path::{Path, PathBuf}};
+use serde::{Serialize, Deserialize};
+
+use crate::tag::Tag;
+
+//HAS LOTS OF WARNINGS!!!!, NOT IMPLEMENTED!!!!!
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct TagFile {
-    
+    workspace_name: String, //Workspace lives as long as the TagFile in-memory
+    #[serde(skip)]
+    full_file_path: PathBuf,
+
+    mapping: HashMap<String, Vec<Tag>>
+}
+
+impl TagFile {
+    pub fn from_file_in_dir(path: &Path) -> TagFile {
+        todo!()
+    }
+
+    pub fn add_tag_to_file_in_self(&mut self, file_name: &Path, tag: &String) {
+        todo!()
+    }
+
+    pub fn remove_tag_from_file_in_self(&mut self, file_name: &Path, tag: &String) {
+        todo!()
+    }
+
+    pub fn get_all_tags(&self) -> HashSet<String> {
+        let mut ret: HashSet<String> = HashSet::<String>::new();
+        self.mapping.values().for_each(|vec | vec.iter().for_each(|tag| {
+            match tag {
+                Tag::Simple(x) => ret.insert(x.to_string()),
+                Tag::KV(k, v) => {
+                    ret.insert(k.to_string());
+                    ret.insert(v.to_string())
+                }
+            };
+        }));
+
+        ret.clone()
+    }
 }
--- tagcore/src/workspace.rs
+++ tagcore/src/workspace.rs
@@ -7,20 +7,21 @@ use crate::errors::TagAddError;
 
 #[derive(Debug)]
 pub struct Workspace {
-    pub root_folder: PathBuf,
-    pub name: String,
-    pub all_tagfiles: HashMap<PathBuf, TagFile>,
-    pub tags_cache: HashSet<String>
+    root_folder: PathBuf,
+    name: String,
+    all_tagfiles: HashMap<PathBuf, TagFile>,
+    tags_cache: HashSet<String>
 }
 
 // Public functions
 impl Workspace {
+    /// Attempts to open a workspace given a directory (a folder) and a workspace name. If no workspace exists in the directory, creates one instead
     pub fn open_or_create_workspace(directory: PathBuf, name: String) -> Result<Workspace, WorkspaceError>{
         if !Workspace::is_name_valid(&name) {
             return Err(WorkspaceError::InvalidName(name));
         }
 
-        let tag_file_name = directory.join(format!(".tag_wrkspc_{}",name));
+        let tag_file_name = directory.join(Workspace::get_workspace_file_name(&name));
 
         //If file cannot be opened, return error
         if let Err(_e) = Workspace::open_workspace_file(&tag_file_name) {
@@ -39,33 +40,41 @@ impl Workspace {
         }) //move into else up above
     }
 
+    /// Scans for .tag files, starting from the workspace's root directory and recursing into folders.
+    pub fn scan_for_tagfiles(&mut self) {
+        use walkdir::WalkDir;
+        let root_folder: String = self.root_folder.to_str().unwrap().to_string();
+        for entry in WalkDir::new(root_folder).into_iter().filter_map(|e| e.ok()) { //Ignores un-owned files
+            // Check if TagFile exists in directory
+            let full_path = entry.path().join(format!(".tag_{}", self.name));
+            if full_path.exists() {
+                //REVIEW - If tag path is IN all_tagfiles hashmap, remove from hasmap and re-serialize it?
+                
+                let tf: TagFile = TagFile::from_file_in_dir(entry.path());
+                self.tags_cache.extend(tf.get_all_tags());
+
+                //add tagfile to workspace's set. This moves the TagFile.
+                self.all_tagfiles.insert(full_path.to_path_buf(), tf);
+            }
+        }
+    }
+
     pub fn add_tag_to_file(&mut self, path_to_file: PathBuf, tag: String) -> Result<(), TagAddError> {
-        // TODO - Check if directory has a .tag file, if not create the file
         let parent_dir: &Path = path_to_file.parent().ok_or(TagAddError::InvalidPath())?;
+        // let file_name = path_to_file.file_name().ok_or(TagAddError::InvalidPath())?;
 
-        //if let Some((exist_path, exist_tagfile)) = self.all_tagfiles.iter().find(|(exist_path,exist_tagfile)| *exist_path == parent_dir) {
-
-        if self.all_tagfiles.contains_key(parent_dir) {
-          // Open the File
-
-        } else {
-            // Create the file
+        if let Some(tf) = self.all_tagfiles.get_mut(parent_dir) {
+            tf.add_tag_to_file_in_self(&path_to_file, &tag);
+        }
+        else {
+            // TODO - Create file
         }
 
-        // FIXME - not implemented yet
-        // let tag_file: TagFile = if parent_dir.exists() && parent_dir.join(self.get_tagfile_name()).exists() {
-        //     // get_tag_file(path_to_file)
-        // } else {
-        //     // create_tag_file(path_to_file)
-        // };
-
-        // Check if tag is in memory-cache, if not, add to cache
+        // Check if tag is in memory-cache, if not, add to cache. only add to cache if TagFile open/create was successful
         if !self.tags_cache.contains(&tag) {
             self.tags_cache.insert(tag);
         }
-        // TODO - Add tag to the TagFile.
 
-        // tag_file.add_tag(&tag);
         Ok(())
     }
 
@@ -101,15 +110,14 @@ impl Workspace {
     // TODO - fix
     fn is_name_valid(name: &String) -> bool {
         let invalid_workspace_name_chars: &str = "/*<>. ";
-        !name.chars().any(|c| invalid_workspace_name_chars.contains(c))
+        !name.chars().any(|c| invalid_workspace_name_chars.contains(c) || c.is_uppercase())
     }
-
     
-    fn get_tagfile_name(&self) -> String {
-        format!(".tag_{}",self.name)
+    fn get_workspace_file_name(name: &String) -> String {
+        format!(".tagwksp_{}",name)
     }
 
-    //Opens a .tag_wks file. The file is just a flag file.
+    //Opens a .tagwksp file. The file is just a flag file.
     fn open_workspace_file(full_path_to_file: &Path) -> std::io::Result<()> {
         match File::create(full_path_to_file) {
             Ok(_file) => Ok(()), //ignore the file handle itself
