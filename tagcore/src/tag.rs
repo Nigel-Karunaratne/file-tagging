2d9784c8058b6a3957f4e90e589d313cd980cb3f
Nigel Karunaratne <nigel.karunaratne36@gmail.com>
2025-11-14 17:31:48 -0500

    Add str handling to Deserialize for Tag, Add tests for Tag serialization

--- tagcore/src/tag.rs
+++ tagcore/src/tag.rs
@@ -31,7 +31,14 @@ impl<'de> Deserialize<'de> for Tag {
                 formatter.write_str("a String or 2-elem tuple of Strings")
             }
 
-            fn visit_string<E>(self, v: String) -> Result<Self::Value, E> //Simple Tag
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> //Simple Tag, un-owned str
+                where
+                E: serde::de::Error,
+            {
+                Ok(Tag::Simple(v.to_owned()))
+            }
+
+            fn visit_string<E>(self, v: String) -> Result<Self::Value, E> //Simple Tag, owned String? unsure if needed
                 where
                     E: serde::de::Error, {
                 Ok(Tag::Simple(v.to_string()))
@@ -44,7 +51,7 @@ impl<'de> Deserialize<'de> for Tag {
                     .ok_or_else(|| serde::de::Error::invalid_length(0, &Self))?;
                 let second: String = seq.next_element()?
                     .ok_or_else(|| serde::de::Error::invalid_length(2, &Self))?;
-                if seq.next_element::<String>()?
+                if seq.next_element::<serde::de::IgnoredAny>()?
                     .is_some() {
                         return Err(serde::de::Error::invalid_length(3, &self));
                 }
@@ -69,5 +76,41 @@ impl Serialize for Tag {
                 pair.serialize(serializer)
             }
         }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use super::*;
+
+    #[test]
+    fn serialize_from_tag_to_str() {
+        // HashMap needed because TOML cannot be represented purely as a string / array
+        let wrapper = HashMap::from( [( "value".to_string(), Tag::Simple("TODO".to_string()) )] );
+        let tag_single_str = toml::to_string(&wrapper).unwrap();
+        assert_eq!(tag_single_str.trim(), "value = \"TODO\"".to_string());
+
+        let wrapper = HashMap::from( [( "value".to_string(), Tag::KV("Due".to_string(), "Today".to_string()) )] );
+        let tag_kv_str = toml::to_string(&wrapper).unwrap();
+        assert_eq!(tag_kv_str.trim(), "value = [\"Due\", \"Today\"]".to_string());
+    }
+
+    #[test]
+    fn deserialize_from_str_to_tag() {
+        #[derive(serde::Deserialize)]
+        struct Wrapper {
+            tag: Tag
+        }
+
+        let simple_tag_str = "tag = \"Phase\"";
+        let simple_tag_wrapper: Wrapper = toml::from_str::<Wrapper>(simple_tag_str).unwrap();
+        assert_eq!(simple_tag_wrapper.tag, Tag::Simple("Phase".to_string()));
+
+        let kv_tag_str = "tag = [\"Duck\",\"Quack\"]";
+        let kv_tag_wrapper: Wrapper = toml::from_str::<Wrapper>(kv_tag_str).unwrap();
+        assert_eq!(kv_tag_wrapper.tag, Tag::KV("Duck".to_string(), "Quack".to_string()));
     }
 }
