ec56606a6a5e7e65bd931fe334a74992b3f00f9e
Nigel Karunaratne <nigel.karunaratne36@gmail.com>
2025-11-17 10:25:45 -0500

    Add python bindings for Workspace

--- tagbinding_py/src/lib.rs
+++ tagbinding_py/src/lib.rs
@@ -1,11 +1,37 @@
 #[pyo3::pymodule]
 mod tags {
     use tagcore;
-    use pyo3::prelude::*;
+    use pyo3::{PyResult, create_exception, prelude::*, pyclass, types::PyType};
 
-    #[pyfunction]
-    fn triple(a: u64, b: u64, c: u64) -> u64 {
-        tagcore::three_mult(a,b,c)
+    create_exception!(tags, PyTagError, pyo3::exceptions::PyException);
+
+    #[pyclass]
+    struct TagWorkspace {
+        inner: tagcore::Workspace,
+    }
+
+    #[pymethods]
+    impl TagWorkspace {
+        #[classmethod]
+        fn open_or_create_workspace(_class: Bound<PyType>, directory: std::path::PathBuf, name: String) -> PyResult<Self> {
+            let x = tagcore::Workspace::open_or_create_workspace(directory, name);
+            match x {
+                Ok(x) => Ok(TagWorkspace {inner: x}),
+                Err(err) => Err(PyTagError::new_err(err.to_string())),
+            }
+        }
+
+        fn scan_for_tagfiles(&mut self) {
+            self.inner.scan_for_tagfiles();
+        }
+
+        fn add_tag_to_file(&mut self, path_to_file: std::path::PathBuf, tag_1: String, tag_2: Option<String>) -> PyResult<()> {
+            self.inner.add_tag_to_file(path_to_file, tag_1, tag_2).map_err(|e| PyTagError::new_err(e.to_string()))
+        }
+
+        fn remove_tag_from_file(&mut self, path_to_file: std::path::PathBuf, tag_1: String, tag_2: Option<String>) -> PyResult<()> {
+            self.inner.remove_tag_from_file(path_to_file, tag_1, tag_2).map_err(|e| PyTagError::new_err(e.to_string()))
+        }
     }
 }
 
--- tagcore/src/lib.rs
+++ tagcore/src/lib.rs
@@ -8,20 +8,5 @@ extern crate tempdir;
 
 // Public interface for library
 pub use workspace::Workspace;
+pub use errors::WorkspaceError;
 
-//*******************************************************************/
-// - keep test binding fn for now
-pub fn three_mult(first: u64, second: u64, third: u64) -> u64 {
-    first * second * third
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = three_mult(2, 7, 8);
-        assert_eq!(result, 112);
-    }
-}
