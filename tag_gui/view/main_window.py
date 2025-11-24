fe0c26c2b6435f46b5b318d9a06b0cc866352cac
Nigel Karunaratne <nigel.karunaratne36@gmail.com>
2025-11-23 19:39:35 -0500

    Swap sides of explore tab, Add scrolling to file info pane

--- tag_gui/view/main_window.py
+++ tag_gui/view/main_window.py
@@ -2,7 +2,7 @@ import sys
 from PySide6.QtCore import Qt, QDir, QModelIndex
 from PySide6.QtGui import QIcon, QAction
 from PySide6.QtWidgets import (
-    QWidget, QTabWidget, QVBoxLayout, QHBoxLayout, QLabel, QSplitter, QFileSystemModel, QTreeView, QMenuBar, QHeaderView, QPushButton, QStackedWidget, QTextEdit
+    QWidget, QTabWidget, QVBoxLayout, QHBoxLayout, QLabel, QSplitter, QFileSystemModel, QTreeView, QMenuBar, QHeaderView, QPushButton, QStackedWidget, QLineEdit, QScrollArea, QSizePolicy
 )
 from model.file_explorer_model import FileExplorerModel
 from model.tag_model import TagModel
@@ -77,48 +77,61 @@ class FilesTab(QWidget):
         self.fs_model = fs_model
         self.tag_model = tag_model
 
-        # ** LEFT SIDE ** #
-        self.left_stack = QStackedWidget()
-        self.left_file_info_widget = FileInfoWidget()
-        self.left_placeholder_widget = QLabel("Select a file to see info...")
-        self.left_stack.addWidget(self.left_placeholder_widget) # Index 0
-        self.left_stack.addWidget(self.left_file_info_widget) # Index 1
-        self.left_stack.setCurrentIndex(0)
+        # ** RIGHT SIDE SIDE ** #
+        self.right_stack = QStackedWidget()
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        self.right_file_info_widget = FileInfoWidget()
+        self.right_placeholder_widget = QLabel("Select a file...")
+        self.right_placeholder_widget.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        self.right_stack.addWidget(self.right_placeholder_widget) # Index 0
+        self.right_stack.addWidget(self.right_file_info_widget) # Index 1
+        self.right_stack.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.MinimumExpanding)
+        scroll_area.resizeEvent = self.on_scroll_resize
+        scroll_area.setWidget(self.right_stack)
+        self.right_stack.setCurrentIndex(0)
 
-        # ** RIGHT SIDE ** #
-        self.right_file_hierarchy = QTreeView()
-        self.right_file_hierarchy.setModel(self.fs_model)
-        self.right_file_hierarchy.setRootIndex(self.fs_model.index(self.fs_model.current_directory))
+        # ** LEFT SIDE ** #
+        self.left_file_hierarchy = QTreeView()
+        self.left_file_hierarchy.setModel(self.fs_model)
+        self.left_file_hierarchy.setRootIndex(self.fs_model.index(self.fs_model.current_directory))
         for col in range(2, self.fs_model.columnCount()):
-            self.right_file_hierarchy.setColumnHidden(col, True)
-        self.right_file_hierarchy.setItemsExpandable(False)
-        self.right_file_hierarchy.setRootIsDecorated(False)
-        header = self.right_file_hierarchy.header()
+            self.left_file_hierarchy.setColumnHidden(col, True)
+        self.left_file_hierarchy.setItemsExpandable(False)
+        self.left_file_hierarchy.setRootIsDecorated(False)
+        header = self.left_file_hierarchy.header()
         header.setSectionResizeMode(0,QHeaderView.ResizeMode.Interactive)
         header.setSectionResizeMode(1,QHeaderView.ResizeMode.ResizeToContents)
         header.setMinimumSectionSize(50)
-        self.right_file_hierarchy.setColumnWidth(0, 250)
-        self.right_file_hierarchy.doubleClicked.connect(self._on_file_folder_double_click)
-        self.right_file_hierarchy.selectionModel().selectionChanged.connect(self._on_file_folder_selection_changed)
+        self.left_file_hierarchy.setColumnWidth(0, 250)
+        self.left_file_hierarchy.doubleClicked.connect(self._on_file_folder_double_click)
+        self.left_file_hierarchy.selectionModel().selectionChanged.connect(self._on_file_folder_selection_changed)
 
-        # self.right_file_hierarchy.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        # self.left_file_hierarchy.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
         # ** ROOT SPLITTER ** #
         splitter_root = QSplitter(Qt.Orientation.Horizontal)
 
-        splitter_root.addWidget(self.left_stack)
-        splitter_root.addWidget(self.right_file_hierarchy)
+        splitter_root.addWidget(self.left_file_hierarchy)
+        splitter_root.addWidget(scroll_area)
 
         layout.addWidget(splitter_root)
-        splitter_root.setSizes([40,300])
+        splitter_root.setSizes([250,40])
+
+    def on_scroll_resize(self, event):
+        self.right_stack.setMinimumWidth(event.size().width())
+        event.accept()
+
 
     def _on_file_folder_selection_changed(self, selected, _deselected):
         indexes = selected.indexes()
         if not indexes or not indexes[0].isValid():
-            self.left_stack.setCurrentIndex(0)
+            self.right_stack.setCurrentIndex(0)
         else:
-            self.left_stack.setCurrentIndex(1)
-            self.left_file_info_widget.set_selected(self.fs_model, indexes[0].siblingAtColumn(0), self.tag_model)
+            self.right_stack.setCurrentIndex(1)
+            self.right_file_info_widget.set_selected(self.fs_model, indexes[0].siblingAtColumn(0), self.tag_model)
         return
 
     def _on_file_folder_double_click(self, index: QModelIndex):
@@ -127,7 +140,7 @@ class FilesTab(QWidget):
         if self.fs_model.isDir(name):
             file_name = self.fs_model.fileName(name)
             self.fs_model.set_directory(file_path, self.tag_model)
-            self.right_file_hierarchy.setRootIndex(self.fs_model.index(file_path))
+            self.left_file_hierarchy.setRootIndex(self.fs_model.index(file_path))
         elif self.fs_model.fileInfo(name).isFile():
             # Platform-Specific code for opening a file
             if sys.platform.startswith("win"): # WINDOWS
@@ -143,7 +156,7 @@ class FilesTab(QWidget):
         parent_dir = os.path.dirname(current_dir)
         if parent_dir and os.path.exists(parent_dir):
             self.fs_model.set_directory(parent_dir, self.tag_model)
-            self.right_file_hierarchy.setRootIndex(self.fs_model.index(parent_dir))
+            self.left_file_hierarchy.setRootIndex(self.fs_model.index(parent_dir))
         
 
 class FileInfoWidget(QWidget):
@@ -158,13 +171,23 @@ class FileInfoWidget(QWidget):
         self.label_size = QLabel()
         self.label_last_modified = QLabel()
 
-        hbox = QHBoxLayout()
-        self.add_tag_text_edit = QTextEdit()
-        self.add_tag_text_edit.setPlaceholderText("tag...")
-        self.add_tag_button = QPushButton("Add Tag")
-        self.add_tag_button.setIcon(QIcon.fromTheme(QIcon.ThemeIcon.ListAdd))
-        hbox.addWidget(self.add_tag_text_edit)
-        hbox.addWidget(self.add_tag_button)
+        # hbox_simple = QHBoxLayout()
+        self.add_simple_tag_text_edit = QLineEdit()
+        self.add_simple_tag_text_edit.setPlaceholderText("tag...")
+        self.add_simple_tag_button = QPushButton("Add Simple Tag")
+        self.add_simple_tag_button.setIcon(QIcon.fromTheme(QIcon.ThemeIcon.ListAdd))
+        # hbox_simple.addWidget(self.add_simple_tag_text_edit)
+        # hbox_simple.addWidget(self.add_simple_tag_button)
+
+        hbox_kv = QHBoxLayout()
+        self.add_kv_tag_k_text_edit = QLineEdit()
+        self.add_kv_tag_k_text_edit.setPlaceholderText("Key...")
+        self.add_kv_tag_v_text_edit = QLineEdit()
+        self.add_kv_tag_v_text_edit.setPlaceholderText("Value...")
+        self.add_kv_tag_button = QPushButton("Add Key-Value Tag")
+        self.add_kv_tag_button.setIcon(QIcon.fromTheme(QIcon.ThemeIcon.ListAdd))
+        hbox_kv.addWidget(self.add_kv_tag_k_text_edit)
+        hbox_kv.addWidget(self.add_kv_tag_v_text_edit)
 
         self.vbox_tags = QVBoxLayout()
         self.label_tags = QLabel("Tags:")
@@ -181,8 +204,11 @@ class FileInfoWidget(QWidget):
         layout.addWidget(self.label_path)
         layout.addWidget(self.label_size)
         layout.addWidget(self.label_last_modified)
-        layout.addLayout(hbox, stretch=0)
         layout.addLayout(self.vbox_tags)
+        layout.addWidget(self.add_simple_tag_text_edit)
+        layout.addWidget(self.add_simple_tag_button)
+        layout.addLayout(hbox_kv, stretch=0)
+        layout.addWidget(self.add_kv_tag_button)
         self.rebuild_tag_list()
 
         self.setLayout(layout)
