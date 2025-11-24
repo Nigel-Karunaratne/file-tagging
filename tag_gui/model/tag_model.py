296b0a0730fbaf4a5b618a93b40eaab6b54c52f4
Nigel Karunaratne <nigel.karunaratne36@gmail.com>
2025-11-23 19:38:46 -0500

    Fix issues with interfacing w/ library

--- tag_gui/model/tag_model.py
+++ tag_gui/model/tag_model.py
@@ -14,7 +14,7 @@ class TagModel():
         print(cwd)
 
     def get_tag_mapping_in_dir_as_strings(self, path_to_directory: str) -> Dict[str, str]:
-        print(f"get_tag_mapping_in_dir_as_strings: {path_to_directory}")
+        print(f"START get_tag_mapping_in_dir_as_strings: {path_to_directory}")
         if self.current_workspace == None:
             return {}
         return_val = {}
@@ -26,14 +26,17 @@ class TagModel():
             tags_as_list = []
             for tag in list_tag:
                 if tag.is_simple():
-                    tags_as_list.append(tag.value)
+                    tags_as_list.append(tag.simple_value)
                 else:
-                    tags_as_list.append(f"{tag.key}: {tag.val}")
-            return_val[entry] = ",".join(tags_as_list)
+                    tags_as_list.append(f"{tag.kv_key}: {tag.kv_value}")
+            print(tags_as_list)
+            return_val[entry] = ", ".join(tags_as_list)
+        
+        print(f"DONE get_tag_mapping_in_dir_as_strings: {path_to_directory}")
         return return_val
         
     def get_tags_for_filename_as_list_of_str(self, path_to_file_name: str) -> list[str]:
-        print(f"get_tags_for_filename_as_list_of_str: {path_to_file_name}")
+        print(f"START get_tags_for_filename_as_list_of_str: {path_to_file_name}")
         # return ["Hello", "123: 124"]
         if self.current_workspace:
             list_tag = self.current_workspace.get_tags_for_file_name(path_to_file_name)
@@ -42,9 +45,10 @@ class TagModel():
             return_list = []
             for tag in list_tag:
                 if tag.is_simple():
-                    return_list.append(tag.value)
+                    return_list.append(tag.simple_value)
                 else:
-                    return_list.append(f"{tag.key}: {tag.val}")
+                    return_list.append(f"{tag.kv_key}: {tag.kv_value}")
+            print(f"DONE get_tags_for_filename_as_list_of_str: {path_to_file_name}")
             return return_list
         else:
             return []
